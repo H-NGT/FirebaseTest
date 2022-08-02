1a295cb7d07f46f30c49503011109685dddaec67
H-NGT <nov.13.v3.0@gmail.com>
2022-08-02 23:03:46 +0900

    Initial Commit

---
+++ FirebaseTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirebaseTest
+//
+//  Created by 永田浩之 on 2022/08/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirebaseTest/FirebaseTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseTestApp.swift
+//  FirebaseTest
+//
+//  Created by 永田浩之 on 2022/08/02.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
